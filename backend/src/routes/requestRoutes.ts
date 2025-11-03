225039aea2a5143da76caa150b66e9122a1065ef
Marc Ivan <mariojaris2@gmail.com>
2025-11-03 20:05:19 +0100

    Small fix / feature X

--- backend/src/routes/requestRoutes.ts
+++ backend/src/routes/requestRoutes.ts
@@ -3,9 +3,9 @@ import { createRequest, getRequests, getRequestById, exportRequestPdf } from '..
 import { ensureAuth } from '../middleware/auth';
 
 const router = Router();
-router.post("/", auth as any, createRequest as any);
-router.get("/", auth as any, getRequests as any);
-router.get("/:id", auth as any, getRequestById as any);
-router.get("/:id/pdf", auth as any, exportRequestPdf as any);
+router.post("/", ensureAuth as any, createRequest as any);
+router.get("/", ensureAuth as any, getRequests as any);
+router.get("/:id", ensureAuth as any, getRequestById as any);
+router.get("/:id/pdf", ensureAuth as any, exportRequestPdf as any);
 
 export default router;
