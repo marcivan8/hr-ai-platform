704ba84c0eea3fe10d85bd5f0f9c675aa439c2ff
Marc Ivan <mariojaris2@gmail.com>
2025-11-04 01:35:59 +0100

    Describe your backend changes

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import { register, login } from '../controllers/authController';
+import * as authController from '../controllers/authController';
 
 const router = Router();
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', authController.register);
+router.post('/login', authController.login);
 
 export default router;
